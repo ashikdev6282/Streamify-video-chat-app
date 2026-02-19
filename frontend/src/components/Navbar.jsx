68cfc285dfec4a65b3200af38c8f821331318303
ashikdev6282 <ashiknandanan@gmail.com>
2026-02-19 23:23:54 +0530

    uselogout change updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link, useLocation } from "react-router";
 import useAuthUser from "../hooks/useAuthUser";
 import { BellIcon, LogOutIcon, ShipWheelIcon } from "lucide-react";
 import ThemeSelector from "./ThemeSelector";
-import useLogout from "../hooks/useLogout.js";
+import { useLogout } from "../hooks/useLogout.js";
 
 const Navbar = () => {
   const { authUser } = useAuthUser();
