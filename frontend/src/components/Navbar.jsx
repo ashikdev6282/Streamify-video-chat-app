86e21efe88bbc47169b067584ce532898c68710f
ashikdev6282 <ashiknandanan@gmail.com>
2026-02-19 22:55:50 +0530

    change on navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,84 +1,56 @@
-import React from "react";
-import useAuthUser from "../hooks/useAuthUser.js";
-import { useLocation, Link } from "react-router-dom";
-import {
-  ShipWheelIcon,
-  BellIcon,
-  LogOutIcon,
-  MenuIcon,
-} from "lucide-react";
-import ThemeSelector from "./ThemeSelector.jsx";
-import { useLogout } from "../hooks/useLogout.js";
+import { Link, useLocation } from "react-router";
+import useAuthUser from "../hooks/useAuthUser";
+import { BellIcon, LogOutIcon, ShipWheelIcon } from "lucide-react";
+import ThemeSelector from "./ThemeSelector";
+import useLogout from "../hooks/useLogout";
 
-
-
-export const Navbar = ({ setIsSidebarOpen }) => {
+const Navbar = () => {
   const { authUser } = useAuthUser();
   const location = useLocation();
-  const isChatPage = location.pathname.startsWith("/chat");
+  const isChatPage = location.pathname?.startsWith("/chat");
+
   const { logoutMutation } = useLogout();
 
   return (
-    <nav className="bg-base-200 border-b border-base-300 h-16 flex items-center px-4 sm:px-6">
-      <div className="flex items-center justify-between w-full">
-
-        {/* LEFT SIDE */}
-        <div className="flex items-center gap-3">
-
-          {/* Mobile Hamburger */}
+    <nav className="bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-end w-full">
+          {/* LOGO - ONLY IN THE CHAT PAGE */}
           {isChatPage && (
-            <button
-              className="md:hidden btn btn-ghost btn-circle"
-              onClick={() => setIsSidebarOpen(true)}
-            >
-              <MenuIcon className="h-6 w-6" />
-            </button>
+            <div className="pl-5">
+              <Link to="/" className="flex items-center gap-2.5">
+                <ShipWheelIcon className="size-9 text-primary" />
+                <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
+                  Streamify
+                </span>
+              </Link>
+            </div>
           )}
 
-          {/* Logo */}
-          {isChatPage && (
-            <Link to="/" className="flex items-center gap-2.5">
-              <ShipWheelIcon className="size-8 text-primary" />
-              <span className="text-xl sm:text-2xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
-                Streamify
-              </span>
+          <div className="flex items-center gap-3 sm:gap-4 ml-auto">
+            <Link to={"/notifications"}>
+              <button className="btn btn-ghost btn-circle">
+                <BellIcon className="h-6 w-6 text-base-content opacity-70" />
+              </button>
             </Link>
-          )}
-        </div>
+          </div>
 
-        {/* RIGHT SIDE */}
-        <div className="flex items-center gap-3 sm:gap-4">
-
-          {/* Notifications */}
-          <Link to={"/notifications"}>
-            <button className="btn btn-ghost btn-circle">
-              <BellIcon className="h-5 w-5 sm:h-6 sm:w-6 text-base-content opacity-70" />
-            </button>
-          </Link>
-
-          {/* Theme */}
+          {/* TODO */}
           <ThemeSelector />
 
-          {/* Avatar */}
           <div className="avatar">
-            <div className="w-8 sm:w-9 rounded-full">
-              <img
-                src={authUser?.profilePic}
-                alt="User Avatar"
-              />
+            <div className="w-9 rounded-full">
+              <img src={authUser?.profilePic} alt="User Avatar" rel="noreferrer" />
             </div>
           </div>
 
-          {/* Logout */}
-          <button
-            className="btn btn-ghost btn-circle"
-            onClick={logoutMutation}
-          >
-            <LogOutIcon className="h-5 w-5 sm:h-6 sm:w-6 text-base-content opacity-70" />
+          {/* Logout button */}
+          <button className="btn btn-ghost btn-circle" onClick={logoutMutation}>
+            <LogOutIcon className="h-6 w-6 text-base-content opacity-70" />
           </button>
         </div>
-
       </div>
     </nav>
   );
 };
+export default Navbar;
